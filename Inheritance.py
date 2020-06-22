78e70b43ffa236ffceefd2ba7bfa2877b56c6695
Sneha Chindarkar <67257891+Sneha07-git@users.noreply.github.com>
2020-06-22 22:43:35 +0530

    Add files via upload
    Example for Multiple Inheritance

--- Inheritance.py
+++ Inheritance.py
@@ -21,12 +21,22 @@ class Archer(User):
         self.num_arrows = num_arrows
         self.arrows_left = arrows_left
 
-    def attack(self):
+    def check_attack(self):
         print(f'attacking with arrows =  {self.arrows_left}- {self.num_arrows}')
 
+#Multiple Inheritance
+class Hybrid(Wizard,Archer):
+    def __init__(self, name,arrows_left,power,num_arrows):
+        Archer.__init__(self,name,num_arrows,arrows_left)
+        Wizard.__init__(self,name,power)
+
+hyb = Hybrid('Hybrid',20,50,100)
+print(hyb.attack())
+print(hyb.check_attack())
+print(hyb.sign_in())
 
 
-
+'''
 wiz1 = Wizard('abc',20)
 arch1 = Archer('pqr',50,40)
 
@@ -34,3 +44,4 @@ wiz1.sign_in()
 arch1.sign_in()
 wiz1.attack()
 arch1.attack()
+'''
