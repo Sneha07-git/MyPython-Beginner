c3a9e57611e7196b0b33006d534e9e5b4717508f
Sneha Chindarkar <67257891+Sneha07-git@users.noreply.github.com>
2020-06-27 16:51:52 +0530

    Translation using Translation Package
    pthonfile.txt - original file in English.
    script.py- Python script
    test-ja.txt - Translated file in Japanese language.

---
+++ script.py
@@ -0,0 +1,13 @@
+from translate import Translator
+
+translator = Translator(to_lang="ja")
+
+try:
+    with open('pythonfile.txt', mode = 'r') as my_file:
+        text = my_file.read()
+        translation = translator.translate(text)
+        print(translation)
+    with open('test-ja.txt', mode = 'w', encoding = 'utf-8') as my_file2:
+        my_file2.write(translation)
+except FileNotFoundError as e:
+    print('Check your file path bozo!!')
